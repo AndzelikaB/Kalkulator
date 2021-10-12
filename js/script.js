7f4f31a99d4d53f0cc04e786f550620a1b72b659
Andzelika Botwina <andzelika.botwina@webanywhere.co.uk>
2021-10-12 22:45:24 +0200

    changes

--- js/script.js
+++ js/script.js
@@ -56,58 +56,58 @@ const historyBtn = document.querySelector('.history-btn');
 
 let result = '';
 
-function displayNumbers(){
-    if(this.textContent === "." && currentNumber.innerHTML.includes('.')) 
-    return;
+function displayNumbers() {
+    if (this.textContent === "." && currentNumber.innerHTML.includes('.'))
+        return;
 
-    if(this.textContent === "." && currentNumber.innerHTML === '') 
-    return currentNumber.innerHTML = ".0";
+    if (this.textContent === "." && currentNumber.innerHTML === '')
+        return currentNumber.innerHTML = ".0";
 
     currentNumber.innerHTML += this.textContent;
 }
 
-function operate(){
-    if(currentNumber.innerHTML === '' && this.textContent === '-')      // currentnumber - jesli pierwszej liczby nie ma  | textContent - jeśli klikniemy minus to 
+function operate() {
+    if (currentNumber.innerHTML === '' && this.textContent === '-')      // currentnumber - jesli pierwszej liczby nie ma  | textContent - jeśli klikniemy minus to 
     {
         currentNumber.innerHTML = '-';
         return;
     }
-    else if(currentNumber.innerHTML === ''){
+    else if (currentNumber.innerHTML === '') {
         return;
     }
 
-    if(mathSign.innerHTML !== ''){
+    if (mathSign.innerHTML !== '') {
         showResult();
     }
-    
+
     previousNumber.innerHTML = currentNumber.innerHTML;
     mathSign.innerHTML = this.textContent;
     currentNumber.innerHTML = '';
 
 }
 
-function showResult(){
-    if(previousNumber.innerHTML=== '' || currentNumber.innerHTML=== '') return currentNumber.innerHTML = "nie podano";
+function showResult() {
+    if (previousNumber.innerHTML === '' || currentNumber.innerHTML === '') return currentNumber.innerHTML = "nie podano";
 
     let a = Number(currentNumber.innerHTML);
     let b = Number(previousNumber.innerHTML);
     let operator = mathSign.innerHTML;
 
-    switch(operator){
+    switch (operator) {
         case '+':
             result = a + b;
             break;
         case '-':
-            result = b - a;   
+            result = b - a;
             break;
         case 'x':
-            result = a * b;  
+            result = a * b;
             break;
         case ':':
-            result = b / a;  
+            result = b / a;
             break;
         case '2^':
-            result = b ** a;  
+            result = b ** a;
             break;
     }
 
@@ -119,24 +119,28 @@ function showResult(){
 
 }
 
-function addToHistory(){
+function addToHistory() {
     const newHistoryItem = document.createElement('li');
     newHistoryItem.innerHTML = `${currentNumber.innerHTML} ${mathSign.innerHTML} ${previousNumber.innerHTML} = ${result}`;
     newHistoryItem.classList.add('history-item');
     calculatorHistory.appendChild(newHistoryItem);
+
 }
 
-function clearScreen(){
+function clearHistory() {
+    calculatorHistory.textContent = '';
+    if (calculatorHistory.textContent === '') {
+        historyBtn.classList.remove('active');
+    }
+}
+
+function clearScreen() {
     result = '';
     currentNumber.innerHTML = '';
     previousNumber.innerHTML = '';
+    mathSign.innerHTML = '';
 }
 
-
-
-function clearHistory(){
-
-}
 
 
 //Nasłuchiwanie przycisków
@@ -145,8 +149,8 @@ operatorsButtons.forEach((button) => button.addEventListener('click', operate));
 equalsButton.addEventListener('click', showResult);
 clearButton.addEventListener('click', clearScreen);
 
-numbersButtons.forEach((button)=>{
-    button.addEventListener('click',displayNumbers);
+numbersButtons.forEach((button) => {
+    button.addEventListener('click', displayNumbers);
 });
 
 historyBtn.addEventListener('click', clearHistory);
